e18277819e261b4d9a75f42fa7cb83c276db61d4
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-05 17:42:28 -0400

    show joins in admin/show for menu & order

--- app/admin/menus.rb
+++ app/admin/menus.rb
@@ -1,3 +1,22 @@
 ActiveAdmin.register Menu do
-  permit_params :name, :bakers_note  
+  permit_params :name, :bakers_note
+
+  show do |menu|
+    attributes_table do
+      row :name
+      row :bakers_note
+      row :menu_items do |menu_items|
+                
+        ul do 
+          menu.menu_items.map do |menu_item|
+            li menu_item.item.name
+          end
+        end
+      end
+      row :created_at
+      row :updated_at
+    end
+
+    active_admin_comments
+  end
 end
--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register Order do
-  permit_params :menu, :user
+  permit_params :feedback, :comments, :menu, :user
+
+  show do |order|
+    attributes_table do
+      row :user
+      row :menu
+      row :feedback
+      row :comments
+      row :order_items do |order_items|
+                
+        ul do 
+          order.order_items.map do |order_items|
+            li order_items.item.name
+          end
+        end
+      end
+      row :created_at
+      row :updated_at
+    end
+
+    active_admin_comments
+  end
 end
