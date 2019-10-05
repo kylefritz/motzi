de9861ceb6f75b2f179968d358cf179b97651c0f
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-04 23:18:03 -0400

    test for menu relationship

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,4 @@
 class Menu < ApplicationRecord
+    has_many :menu_items
     has_many :items, through: :menu_items
 end
--- test/models/menu_test.rb
+++ test/models/menu_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MenuTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "items connected to menu through menu_items" do
+    week1 = menus(:week1)
+    assert_equal week1.name, 'week1'
+    assert_equal week1.items.count, 2
+  end
+
+  test "menus can have add_ons" do
+    week2 = menus(:week2)
+    assert_equal week2.items.count, 3, 'three items'
+    add_ons = week2.menu_items.select {|i| i.is_add_on?}
+    assert_equal add_ons.count, 1, '1 add on (donuts)'
+    assert_equal add_ons.first.item, items(:donuts)
+  end
 end
