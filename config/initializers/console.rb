0ddb7d50e91ea03058bf2ac1a2740579226e60db
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-06 14:01:12 -0400

    in rails console, save papertrail user_id

---
+++ config/initializers/console.rb
@@ -0,0 +1,19 @@
+# https://github.com/paper-trail-gem/paper_trail/wiki/Setting-whodunnit-in-the-rails-console
+Rails.application.configure do
+  console do
+    PaperTrail.request.whodunnit = ->() {
+      @paper_trail_whodunnit ||= (
+        kyle_id   = User.find_by(email: 'kyle.p.fritz@gmail.com')&.id
+        adrian_id = User.find_by(email: 'adrian.alday@gmail.com')&.id
+
+        user_id = nil
+        until user_id.present? do
+          print "Attribute change to which user_id [kyle: #{kyle_id} adrian: #{adrian_id}]? "
+          user_id = gets.chomp
+        end
+        puts "Papertrail user_id: #{user_id}"
+        user_id
+      )
+    }
+  end
+end
