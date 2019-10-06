3efacf81315831e19f2e8b2676ce767af3a4242f
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-06 13:56:55 -0400

    slicker whodunnit dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,12 +2,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+
 
   section "Recently updated content" do
     table_for PaperTrail::Version.order('id desc').limit(20) do # Use PaperTrail::Version if this throws an error
@@ -15,7 +10,9 @@ ActiveAdmin.register_page "Dashboard" do
       # column ("Item") { |v| link_to v.item, [:admin, v.item] } # Uncomment to display as link
       column ("Type") { |v| v.item_type.underscore.humanize }
       column ("Modified at") { |v| v.created_at.to_s :long }
-      column ("User") { |v| User.find_by(id: v.whodunnit)&.first_name }
+      column ("User") do |v|
+        User.find_by(id: v.whodunnit)&.first_name || "whodunnit: '#{v.whodunnit}'"
+      end
     end
   end
 
