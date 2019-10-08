e6f3049631133e689929946e439141d58e4c60d5
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-07 22:46:17 -0400

    fix menu mailer preview?

--- app/mailers/previews/menu_mailer_preview.rb
+++ app/mailers/previews/menu_mailer_preview.rb
@@ -1,5 +1,5 @@
-class MenuMailerPreview < ActionMailer::Preview
-  def menu_email
-    MenuMailer.with(menu: @menu).menu_email
+class Previews::MenuMailerPreview < ActionMailer::Preview
+  def weekly_menu
+    MenuMailer.with(menu: @menu, user: @user).menu_email
   end
 end
