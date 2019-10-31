365fc00324837f169e35f7898f588823c15f9d38
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-31 16:46:07 -0400

    markdown bakers note in email

--- app/mailers/menu_mailer.rb
+++ app/mailers/menu_mailer.rb
@@ -2,6 +2,9 @@ class MenuMailer < ApplicationMailer
   def weekly_menu
     @menu = params[:menu]
     @user = params[:user]
+    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    @bakers_note = renderer.render(@menu.bakers_note).html_safe
+
     mail(to: "#{@user.name} <#{@user.email}>", cc: @user.additional_email, subject: "Motzi Bread - #{@menu.name}")
   end
 end
