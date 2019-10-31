02c7d00b7b2b72b7140638d4fa2ba4af82d1ebd4
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-10-30 23:03:50 -0400

    fix lable for add-ons

--- app/javascript/packs/menu/AddOn.js
+++ app/javascript/packs/menu/AddOn.js
@@ -13,13 +13,13 @@ export default class AddOn extends React.Component {
     this.props.onChange(this.cbRef.current.checked)
   }
   render() {
-    const { name, description, image } = this.props;
+    const { name } = this.props;
 
     return (
       <div className="form-check mb-1">
-        <input value={name} ref={this.cbRef} onChange={this.handleChanged.bind(this)}
-          className="form-check-input" type="checkbox" />
         <label className="form-check-label">
+          <input value={name} ref={this.cbRef} onChange={this.handleChanged.bind(this)}
+            className="form-check-input" type="checkbox" />
           {name}
         </label>
       </div>
