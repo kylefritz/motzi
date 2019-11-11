6cb72d6aa1511280125919ad22c278328dfebbbf
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-11-11 13:45:54 -0500

    better mailer variables

--- app/mailers/menu_mailer.rb
+++ app/mailers/menu_mailer.rb
@@ -3,8 +3,10 @@ class MenuMailer < ApplicationMailer
     @menu = params[:menu]
     @user = params[:user]
     renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-    @bakers_note = renderer.render(@menu.bakers_note).html_safe
+    @bakers_note_html = renderer.render(@menu.bakers_note).html_safe
 
-    mail(to: "#{@user.name} <#{@user.email}>", cc: @user.additional_email, subject: "Motzi Bread - #{@menu.name}")
+    mail(to: %("#{@user.name}" <#{@user.email}>),
+         cc: @user.additional_email,
+         subject: "Motzi Bread - #{@menu.name}")
   end
 end
