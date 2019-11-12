f51d1fcf32bc5353470e12c9eb8d46e2acb98224
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-11-11 19:31:25 -0500

    messages table

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -1,5 +1,23 @@
 ActiveAdmin.register Ahoy::Message, as: "Email" do
   actions :index, :show
+  includes :menu, :user
   scope("all") { |scope| scope }
   scope("current menu") { |scope| scope.where(menu_id: Setting.menu_id) }
+
+  index do
+    column :to do |email|
+      div auto_link email.to
+    end
+    column :user do |email|
+      div auto_link email.user
+    end
+    column "menu" do |email|
+      auto_link email.menu
+    end
+    column :sent_at
+    column :opened_at
+    column :clicked_at
+    column :subject
+    # column :mailer
+  end
 end
