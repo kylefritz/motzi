038b8bedbf9a0b89ded84f59ef6dec07ea94f8fd
Kyle Fritz <kyle.p.fritz@gmail.com>
2019-11-16 11:32:51 -0500

    hashid dont override to_param

--- config/initializers/hashid.rb
+++ config/initializers/hashid.rb
@@ -4,4 +4,13 @@ Hashid::Rails.configure do |config|
 
   # The minimum length of generated hashids
   config.min_hash_length = 4
+
+  # Whether to override the `find` method
+  # config.override_find = true
+
+  # Whether to override the `to_param` method
+  config.override_to_param = false
+
+  # Whether to sign hashids to prevent conflicts with regular IDs (see https://github.com/jcypret/hashid-rails/issues/30)
+  # config.sign_hashids = true
 end
