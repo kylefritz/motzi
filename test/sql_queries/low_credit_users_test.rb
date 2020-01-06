98504fe466ce441a94e098ed137ae8bd8d734815
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-01-06 16:11:42 -0500

    Bug counted skips in low_credit_users query. Fixes #78

--- test/sql_queries/low_credit_users_test.rb
+++ test/sql_queries/low_credit_users_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LowCreditUsersTest < ActiveSupport::TestCase
   test "low credit users" do
-    rows = SqlQuery.new(:low_credit_users, balance: 4).execute
+    rows = exec_low_credit_users
     assert_equal 2, rows.size
     low_credit_users = [users(:ljf), users(:jess)]
     user_ids = Set[rows.map {|u| u["user_id"]}]
@@ -10,5 +10,33 @@ class LowCreditUsersTest < ActiveSupport::TestCase
 
     balances = Set[rows.map {|u| u["credit_balance"]}]
     assert_equal Set[low_credit_users.map(&:credits)], balances
+  end
+
+  test "doesnt charge for skip" do
+    kyle = users(:kyle)
+    menu = menus(:week2)
+
+    # synthesize enough orders that kyle will be in the "low-credit" list
+    _, items = menu.menu_items.partition(&:is_add_on?)
+    new_kyle_orders = kyle.credits.times.map do
+      kyle.orders.create!(menu: menu).tap do |order|
+        order.order_items.create!(item: items.sample.item)
+      end
+    end
+    assert_equal 0, kyle.credits, 'now kyle has no credits'
+    assert_equal 3, exec_low_credit_users.size, 'kyle included in low credit users'
+    
+    # make all the new orders into *skip*, shouldnt charge user
+    new_kyle_orders.each do |order|
+      oi = order.order_items.first
+      oi.update!(item_id: Item::SKIP_ID)
+    end
+    refute_equal 0, kyle.credits, 'kyle should have credits again'
+    assert_equal 2, exec_low_credit_users.size, 'kyle is not in low credit users'
+  end
+
+  private
+  def exec_low_credit_users
+    SqlQuery.new(:low_credit_users, balance: 4).execute
   end
 end
