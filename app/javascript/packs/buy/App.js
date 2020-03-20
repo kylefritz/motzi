06da495a4b287858090bc980a730de5232c2551f
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-03-20 14:16:16 -0400

    css looking better; fixing payment request issue

--- app/javascript/packs/buy/App.js
+++ app/javascript/packs/buy/App.js
@@ -26,13 +26,18 @@ export default function Buy() {
     const { uid } = queryString.parse(location.search);
     const params = { uid };
     axios.get("/menu.json", { params }).then(({ data: {user} }) => {
-      setUser(user)
-      Sentry.configureScope(scope => scope.setUser(user));
+      if(user)
+      {
+        setUser(user);
+        Sentry.configureScope(scope => scope.setUser(user));
+      }else{
+        setError("We can't load your user account");
+      }
     })
     .catch(error => {
       console.error("cant load user from menu page", error);
       Sentry.captureException(error);
-      setError("We can't load the menu");
+      setError("We can't load your user account");
     });
   }, []);
 
@@ -62,19 +67,18 @@ export default function Buy() {
     // TODO: send completed payment request to rails
   };
 
-  if(!user)
-  {
-    return <div>Loading subscription renewal...</div>
-  }
-
   if(error)
   {
-    return <div>Loading subscription renewal...</div>
+    return <div>{error}</div>
+  }
+
+  if (!user) {
+    return <div>Loading user for subscription renewal...</div>;
   }
 
   return (
     <div className="alert alert-info" role="alert">
-      <h2>Renew your subscription</h2>
+      <h2 className="mb-3">Renew your subscription</h2>
       <Choice
         name="Weekly"
         price={weeklyPrice}
@@ -82,19 +86,25 @@ export default function Buy() {
         defaultChecked={true}
       />
       <Choice name="Bi-Weekly" price={biweeklyPrice} onChoose={setChoice} />
-      <Choice name="I don't want to renew" value="dont" onChoose={setChoice} />
-      {choice &&
-        (dont ? (
-          <Feedback />
-        ) : (
-          <Payment
-            choice={choice}
-            price={price}
-            stripeApiKey={stripeApiKey}
-            onCardToken={handleCardToken}
-            onPaymentResult={handlePaymentResult}
-          />
-        ))}
+      <h4>Payment amount</h4>
+      <p>
+        <small>
+          These are suggested prices based on the our costs. We want everyone to
+          have access to healthy food in this time of crisis and beyond,
+          regardless of ability to pay. If you are out of work or otherwise in a
+          challenging financial situation please pay what you can or nothing at
+          all. If you have the means to pay more we would welcome your support
+          in providing for the community and ensuring our continued stability as
+          a small business.
+        </small>
+      </p>
+      <Payment
+        choice={choice}
+        price={price}
+        stripeApiKey={stripeApiKey}
+        onCardToken={handleCardToken}
+        onPaymentResult={handlePaymentResult}
+      />
     </div>
   );
 }
--- app/javascript/packs/buy/PaymentRequest.js
+++ app/javascript/packs/buy/PaymentRequest.js
@@ -1,36 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { PaymentRequestButtonElement } from 'react-stripe-elements'
 
 // The Payment Request Button is a single integration that allows you to accept Apple Pay,
 // Google Pay, Microsoft Pay, and the Payment Request API.
 export default function PaymentRequest({ stripe, onToken, choice, price }) {
-  const [paymentRequest, setPaymentRequest] = useState(() => {
+  const [paymentRequest, setPaymentRequest] = useState()
+  useEffect(() => {
     // For full documentation of the available paymentRequest options, see:
     // https://stripe.com/docs/stripe.js#the-payment-request-object
-    return stripe.paymentRequest({
-      country: 'US',
-      currency: 'usd',
+    const pr = stripe.paymentRequest({
+      country: "US",
+      currency: "usd",
       total: {
         label: choice,
-        amount: price,
+        amount: price
       },
       // Requesting the payer’s name, email, or phone is optional, but recommended.
       // It also results in collecting their billing address for Apple Pay.
       requestPayerName: true,
-      requestPayerEmail: true,
-    })
-  })
+      requestPayerEmail: true
+    });
+
+    pr.canMakePayment().then(result => {
+      console.log("paymentRequest supported?", !!result);
+      setCanMakePayment(!!result);
+    });
+
+    pr.on("token", ({ complete, token, ...data }) => {
+      onToken({ token, data });
+      complete("success");
+    });
+
+    setPaymentRequest(pr);
+  }, [])
+
   const [canMakePayment, setCanMakePayment] = useState(false)
-
-  paymentRequest.on('token', ({ complete, token, ...data }) => {
-    onToken({ token, data })
-    complete('success')
-  })
-
-  paymentRequest.canMakePayment().then(result => {
-    console.log("paymentRequest supported?", !!result);
-    setCanMakePayment(!!result)
-  })
 
   if (!canMakePayment) {
     return (
@@ -43,7 +47,7 @@ export default function PaymentRequest({ stripe, onToken, choice, price }) {
   // For more details on how to style the Payment Request Button, see:
   // https://stripe.com/docs/elements/payment-request-button#styling-the-element
   return <PaymentRequestButtonElement
-    paymentRequest={this.state.paymentRequest}
+    paymentRequest={paymentRequest}
     className="PaymentRequestButton"
     style={{ paymentRequestButton: { theme: 'light', height: '64px' } }}
   />
