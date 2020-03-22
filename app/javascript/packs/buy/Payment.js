9e152314c184c89dde8cb82b0905f6da2d14b536
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-03-22 17:25:54 -0400

    disable payment request for now

--- app/javascript/packs/buy/Payment.js
+++ app/javascript/packs/buy/Payment.js
@@ -6,23 +6,25 @@ import {
 } from 'react-stripe-elements';
 
 import Card from './Card'
-import PaymentRequest from './PaymentRequest'
-
-const WrappedPaymentRequest = injectStripe(PaymentRequest);
 const WrappedCard = injectStripe(Card);
+
+/*
+import PaymentRequest from "./PaymentRequest";
+const WrappedPaymentRequest = injectStripe(PaymentRequest);
+<StripeProvider apiKey={stripeApiKey}>
+  <Elements>
+    <WrappedPaymentRequest
+      onToken={onPaymentResult}
+      credits={credits}
+      price={price}
+    />
+  </Elements>
+</StripeProvider>;
+*/
 
 export default function Payment({ credits, price, stripeApiKey, onCardToken, onPaymentResult }) {
   return (
     <>
-      <StripeProvider apiKey={stripeApiKey}>
-        <Elements>
-          <WrappedPaymentRequest
-            onToken={onPaymentResult}
-            credits={credits}
-            price={price}
-          />
-        </Elements>
-      </StripeProvider>
       <StripeProvider apiKey={stripeApiKey}>
         <Elements>
           <WrappedCard onToken={onCardToken} price={price} credits={credits} />
