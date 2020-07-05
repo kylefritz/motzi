f103c93582d7edf42404ce88d8855deac4e1f936
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-07-05 13:54:48 -0400

    Create site_id setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -9,6 +9,15 @@ class Setting < RailsSettings::Base
   field :automated_reminder_emails, default: true, type: :boolean
   field :pickup_day1, default: "Tuesday", type: :string
   field :pickup_day2, default: "Thursday", type: :string
+  field :site_id, default: "motzi", type: :string
+
+  def self.site
+    if site_id == "motzi"
+      "Motzi Bread"
+    else
+      "Jinji Chocolate"
+    end
+  end
 
   def self.pickup_day1_abbr
     abbr_day(Setting.pickup_day1)
