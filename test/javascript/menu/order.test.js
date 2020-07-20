0d57ef43dbe7737c9f9ab9f95251d91b1bb639d3
Kyle Fritz <kyle.p.fritz@gmail.com>
2020-07-20 18:45:25 -0400

    get rid of last dumb snapshot test

--- test/javascript/menu/order.test.js
+++ test/javascript/menu/order.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 require("../configure_enzyme");
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
+import { munge } from "menu/App";
 import Order from "menu/Order";
 import orderData from "./order-data";
 
 test("Order snapshot", () => {
-  const wrapper = shallow(<Order {...orderData} />);
-  expect(wrapper).toMatchSnapshot();
+  const { user, order, menu } = orderData;
+  const wrapper = mount(<Order {...{ user, order, menu: munge(menu) }} />);
+
+  expect(wrapper.find("DaysCart").length).toBe(1);
+  expect(wrapper.find("DaysCart").text()).toContain("Classic");
+  expect(wrapper.find("h3").text()).toContain("got your order");
 });
