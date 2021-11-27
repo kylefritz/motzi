36c9f9eff8ffe5b2097a5149c8d1615dcb69ea15
Kyle Fritz <kyle.p.fritz@gmail.com>
2021-11-26 23:51:04 -0500

    add dependent: :destroy for menu_item pickup days

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :item
   belongs_to :menu
-  has_many :menu_item_pickup_days
+  has_many :menu_item_pickup_days, dependent: :destroy
   has_paper_trail
 end
