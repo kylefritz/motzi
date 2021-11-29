1d5db4c5bd53f7899f8c6933b9ce44d1ff220dff
Kyle Fritz <kyle.p.fritz@gmail.com>
2021-11-28 21:55:42 -0500

    improve email formatting of pickup days/deadlines

--- test/mailers/previews/reminder_mailer_preview.rb
+++ test/mailers/previews/reminder_mailer_preview.rb
@@ -1,9 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/reminder_mailer
 class ReminderMailerPreview < ActionMailer::Preview
   def day_of_email
+    order_items = Order.last.order_items
     menu = Menu.current
     user = User.last
-    ReminderMailer.with(menu: menu, user: user).day_of_email
+    ReminderMailer.with(menu: menu, user: user, order_items: order_items).day_of_email
   end
 
   def havent_ordered_email
