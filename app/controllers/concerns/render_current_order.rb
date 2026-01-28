e7f83233875c69837c02a43934976d6136fe534b
Kyle Fritz <kyle.p.fritz@gmail.com>
2026-01-28 11:25:53 -0500

    Keep menu.json defaulting to current menu

--- app/controllers/concerns/render_current_order.rb
+++ app/controllers/concerns/render_current_order.rb
@@ -5,7 +5,7 @@ module RenderCurrentOrder
     protected
 
     def render_current_order(menu_id=nil, user=nil)
-      @menu = menu_id ? Menu.find(menu_id) : Menu.open_for_ordering.first || Menu.current
+      @menu = menu_id ? Menu.find(menu_id) : Menu.current
       @open_menus = Menu.open_for_ordering.to_a
       @open_menus << @menu if @menu.present? && !@open_menus.include?(@menu)
       @open_menus = @open_menus.uniq.sort_by { |menu| menu.week_id.to_s.downcase }.reverse
