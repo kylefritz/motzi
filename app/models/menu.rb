a8ae7a4c37bdc5e2131f6c38eea07a43a3914a77
Kyle Fritz <kyle.p.fritz@gmail.com>
2026-02-03 01:18:35 -0500

    Clarify Menu#ordering_window logic

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -5,9 +5,9 @@ class Menu < ApplicationRecord
   has_many :items, through: :menu_items
   has_many :orders
   has_many :order_items, through: :orders
-  has_many :messages, class_name: "Ahoy::Message"
+  has_many :messages, class_name: 'Ahoy::Message'
   has_paper_trail
-  default_scope { order("LOWER(week_id) desc") }
+  default_scope { order('LOWER(week_id) desc') }
 
   def self.current
     Menu.find(Setting.menu_id)
@@ -15,15 +15,16 @@ class Menu < ApplicationRecord
 
   def self.open_for_ordering
     Menu.joins(:pickup_days)
-        .group("menus.id")
-        .having("MAX(pickup_days.order_deadline_at) >= ?", Time.zone.now)
+        .group('menus.id')
+        .having('MAX(pickup_days.order_deadline_at) >= ?', Time.zone.now)
   end
+
   def self.for_current_week_id
     Menu.find_by(week_id: Time.zone.now.week_id)
   end
 
   def make_current!
-    Setting.menu_id = self.id
+    Setting.menu_id = id
   end
 
   def week_start
@@ -41,7 +42,7 @@ class Menu < ApplicationRecord
   end
 
   def for_current_week_id?
-    self.week_id == Time.zone.now.week_id
+    week_id == Time.zone.now.week_id
   end
 
   def allow_overlap?
@@ -49,62 +50,56 @@ class Menu < ApplicationRecord
   end
 
   def can_publish?
-    self.week_id >= Time.zone.now.week_id
+    week_id >= Time.zone.now.week_id
   end
 
   def item_counts
     # TODO: how should we represent this?
-    day_item_counts = SqlQuery.new(:ordered_items_counts, menu_id: self.id).execute
+    day_item_counts = SqlQuery.new(:ordered_items_counts, menu_id: id).execute
     # pickup_days = self.pickup_days.all
 
     {}.tap do |counts|
       day_item_counts.each do |r|
-        pickup_day_id = r["pickup_day_id"]
-        item_id = r["item_id"]
-        num_items = r["sum"]
+        pickup_day_id = r['pickup_day_id']
+        item_id = r['item_id']
+        num_items = r['sum']
 
-        if counts[item_id].nil?
-          counts[item_id] = {}
-        end
+        counts[item_id] = {} if counts[item_id].nil?
         counts[item_id][pickup_day_id] = num_items
       end
     end
   end
 
   def item_counts_by_pickup_day
-    day_item_counts = SqlQuery.new(:ordered_items_counts, menu_id: self.id).execute
+    day_item_counts = SqlQuery.new(:ordered_items_counts, menu_id: id).execute
     {}.tap do |counts|
       day_item_counts.each do |r|
-        pickup_day_id = r["pickup_day_id"]
-        item_id = r["item_id"]
-        num_items = r["sum"]
+        pickup_day_id = r['pickup_day_id']
+        item_id = r['item_id']
+        num_items = r['sum']
 
-        if counts[pickup_day_id].nil?
-          counts[pickup_day_id] = {}
-        end
+        counts[pickup_day_id] = {} if counts[pickup_day_id].nil?
         counts[pickup_day_id][item_id] = num_items
       end
     end
   end
 
   def subscriber_note_html
-    @subscriber_note_html ||= MARKDOWN.render(self.subscriber_note || '').html_safe
+    @subscriber_note_html ||= MARKDOWN.render(subscriber_note || '').html_safe
   end
 
   def menu_note_html
-    @menu_note_html ||= MARKDOWN.render(self.menu_note || '').html_safe
+    @menu_note_html ||= MARKDOWN.render(menu_note || '').html_safe
   end
 
   def day_of_note_html
-    @day_of_note_html ||= MARKDOWN.render(self.day_of_note || '').html_safe
+    @day_of_note_html ||= MARKDOWN.render(day_of_note || '').html_safe
   end
 
   def publish_to_subscribers!
-    unless can_publish?
-      throw "can only publish_to_subscribers for current week's menu or future week's menu"
-    end
-    self.make_current!
-    self.touch :emailed_at # create audit that email was sent
+    throw "can only publish_to_subscribers for current week's menu or future week's menu" unless can_publish?
+    make_current!
+    touch :emailed_at # create audit that email was sent
     SendWeeklyMenuJob.users_to_email_count(self).tap do
       # Pass the specific menu ID to the job
       SendWeeklyMenuJob.perform_later(id)
@@ -114,6 +109,7 @@ class Menu < ApplicationRecord
   def ordering_closed?
     deadline = latest_deadline
     return false if deadline.nil?
+
     Time.zone.now > deadline
   end
 
@@ -125,7 +121,7 @@ class Menu < ApplicationRecord
     if pickup_days.loaded?
       pickup_days.map(&:order_deadline_at).min
     else
-      self.pickup_days.minimum(:order_deadline_at)
+      pickup_days.minimum(:order_deadline_at)
     end
   end
 
@@ -133,23 +129,24 @@ class Menu < ApplicationRecord
     if pickup_days.loaded?
       pickup_days.map(&:order_deadline_at).max
     else
-      self.pickup_days.maximum(:order_deadline_at)
+      pickup_days.maximum(:order_deadline_at)
     end
   end
 
   def ordering_window(deadlines: nil, enforce_week: nil)
     values = deadlines
     values ||= if pickup_days.loaded?
-      pickup_days.map(&:order_deadline_at)
-    else
-      pickup_days.pluck(:order_deadline_at)
-    end
+                 pickup_days.map(&:order_deadline_at)
+               else
+                 pickup_days.pluck(:order_deadline_at)
+               end
 
     return nil if values.blank?
 
-    enforce_week = !is_special? if enforce_week.nil?
-    if enforce_week
-      week_start = Time.zone.from_week_id(self.week_id)
+    should_enforce_week = enforce_week.nil? ? !is_special? : enforce_week
+
+    if should_enforce_week
+      week_start = Time.zone.from_week_id(week_id)
       week_end = week_start + 7.days
       values = values.select { |deadline| deadline >= week_start && deadline <= week_end }
       return nil if values.empty?
@@ -167,12 +164,10 @@ class Menu < ApplicationRecord
   end
 
   def sorted_menu_items(includes: nil)
-    my_menu_items = self.menu_items
-    if includes.present?
-      my_menu_items = my_menu_items.includes(includes)
-    end
+    my_menu_items = menu_items
+    my_menu_items = my_menu_items.includes(includes) if includes.present?
 
-    my_menu_items.sort_by {|mi| mi.sort_order.nil? ? 1_000 : mi.sort_order}
+    my_menu_items.sort_by { |mi| mi.sort_order.nil? ? 1_000 : mi.sort_order }
   end
 
   def copy_from(original_menu, options = {})
@@ -181,20 +176,20 @@ class Menu < ApplicationRecord
     pickup_day_map = map_pickup_days_from(original_menu)
 
     # if there are already items in this menu, append these new ones to the end by offsetting the sort order index
-    sort_offset = self.menu_items.pluck(:sort_order).compact.max || 0
+    sort_offset = menu_items.pluck(:sort_order).compact.max || 0
 
     original_menu.sorted_menu_items.each_with_index do |original_mi, index|
-      new_mi = self.menu_items.create!(
+      new_mi = menu_items.create!(
         item_id: original_mi.item_id,
         subscriber: original_mi.subscriber,
         marketplace: original_mi.marketplace,
-        sort_order: sort_offset + index,
+        sort_order: sort_offset + index
       )
 
       original_mi.menu_item_pickup_days.each do |mipud|
         new_mi.menu_item_pickup_days.create!(
           pickup_day_id: pickup_day_map[mipud.pickup_day_id],
-          limit: mipud.limit,
+          limit: mipud.limit
         )
       end
     end
@@ -204,9 +199,9 @@ class Menu < ApplicationRecord
     # we intentionally don't support multiple pickup days on the same weekday
 
     original_week_start = Time.zone.from_week_id(original_menu.week_id)
-    target_week_start = Time.zone.from_week_id(self.week_id)
+    target_week_start = Time.zone.from_week_id(week_id)
 
-    target_days_by_wday = self.pickup_days.index_by { |day| day.pickup_at.wday }
+    target_days_by_wday = pickup_days.index_by { |day| day.pickup_at.wday }
 
     {}.tap do |map|
       original_menu.pickup_days.each do |pickup_day|
@@ -214,7 +209,7 @@ class Menu < ApplicationRecord
         if target_day.nil?
           target_day = pickup_days.create!(
             pickup_at: target_week_start + (pickup_day.pickup_at - original_week_start),
-            order_deadline_at: target_week_start + (pickup_day.order_deadline_at - original_week_start),
+            order_deadline_at: target_week_start + (pickup_day.order_deadline_at - original_week_start)
           )
           target_days_by_wday[pickup_day.pickup_at.wday] = target_day
         end
@@ -228,15 +223,9 @@ class Menu < ApplicationRecord
     attrs = {}
 
     # for each note, only copy if note is empty in this menu
-    if options[:copy_subscriber_note] && subscriber_note.blank?
-      attrs[:subscriber_note] = original_menu.subscriber_note
-    end
-    if options[:copy_menu_note] && menu_note.blank?
-      attrs[:menu_note] = original_menu.menu_note
-    end
-    if options[:copy_day_of_note] && day_of_note.blank?
-      attrs[:day_of_note] = original_menu.day_of_note
-    end
+    attrs[:subscriber_note] = original_menu.subscriber_note if options[:copy_subscriber_note] && subscriber_note.blank?
+    attrs[:menu_note] = original_menu.menu_note if options[:copy_menu_note] && menu_note.blank?
+    attrs[:day_of_note] = original_menu.day_of_note if options[:copy_day_of_note] && day_of_note.blank?
 
     update!(attrs) if attrs.any?
   end
