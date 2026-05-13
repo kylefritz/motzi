6b347a65aa1ea3b08196f4b49a4ce0699d955440
Kyle Fritz <kyle.p.fritz@gmail.com>
2026-05-12 21:11:03 -0400

    Port /about (Our Process) page from Wix

--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -14,4 +14,12 @@ class AboutControllerTest < ActionDispatch::IntegrationTest
     get "/about"
     assert_response :success
   end
+
+  test "renders the Our Process heading and key sections" do
+    get "/about"
+    assert_select "h1", text: /Our Process/i
+    assert_select "h2", text: /Local Sourcing/i
+    assert_select "h2", text: /Fresh Milling/i
+    assert_select "h2", text: /Long Fermentation/i
+  end
 end
