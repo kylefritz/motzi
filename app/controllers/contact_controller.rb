9bc60fdd90cb00d71809eea033c383e7fc37be6f
Kyle Fritz <kyle.p.fritz@gmail.com>
2026-05-12 21:17:00 -0400

    Add contact form: persist ContactMessage with honeypot, render errors

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,4 +5,27 @@ class ContactController < ApplicationController
   def show
     @message = ContactMessage.new
   end
+
+  def create
+    if params.dig(:contact_message, :website).present?
+      redirect_to "/contact", notice: "Thanks! We'll be in touch."
+      return
+    end
+
+    @message = ContactMessage.new(contact_message_params)
+    @message.ip = request.remote_ip
+    @message.user_agent = request.user_agent
+
+    if @message.save
+      redirect_to "/contact", notice: "Thanks! We'll be in touch."
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def contact_message_params
+    params.require(:contact_message).permit(:name, :email, :phone, :message)
+  end
 end
--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -22,4 +22,50 @@ class ContactControllerTest < ActionDispatch::IntegrationTest
     assert_match /443-272-1515/, @response.body
     assert_match /Tues.*Sat/i, @response.body
   end
+
+  test "show renders the form" do
+    get "/contact"
+    assert_select "form[action=?][method=?]", "/contact", "post"
+    assert_select "input[name='contact_message[name]']"
+    assert_select "input[name='contact_message[email]']"
+    assert_select "input[name='contact_message[phone]']"
+    assert_select "textarea[name='contact_message[message]']"
+  end
+
+  test "create with valid params persists a ContactMessage and redirects" do
+    assert_difference -> { ContactMessage.count }, 1 do
+      post "/contact", params: { contact_message: {
+        name: "Maya", email: "maya@example.com", phone: "555-1212", message: "Hello!"
+      } }
+    end
+    assert_redirected_to "/contact"
+    follow_redirect!
+    assert_match /thanks/i, @response.body
+  end
+
+  test "create captures ip and user_agent" do
+    post "/contact", params: { contact_message: {
+      name: "X", email: "x@y.com", message: "hi"
+    } }, headers: { "User-Agent" => "TestBot/1.0" }
+    msg = ContactMessage.order(:created_at).last
+    assert_equal "TestBot/1.0", msg.user_agent
+    assert_not_nil msg.ip
+  end
+
+  test "create with invalid params re-renders show with 422" do
+    assert_no_difference -> { ContactMessage.count } do
+      post "/contact", params: { contact_message: { name: "", email: "", message: "" } }
+    end
+    assert_response :unprocessable_entity
+    assert_select "form[action=?]", "/contact"
+  end
+
+  test "honeypot field silently swallows submission" do
+    assert_no_difference -> { ContactMessage.count } do
+      post "/contact", params: { contact_message: {
+        name: "Spammer", email: "spam@bad.com", message: "buy stuff", website: "http://bad.example"
+      } }
+    end
+    assert_redirected_to "/contact"
+  end
 end
