07c927c80ce7aa155dd79ee6b05b24308576b64f
Kyle Fritz <kyle.p.fritz@gmail.com>
2026-05-12 21:14:34 -0400

    Port /contact static info (address, phone, hours) from Wix

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -14,4 +14,12 @@ class ContactControllerTest < ActionDispatch::IntegrationTest
     get "/contact"
     assert_response :success
   end
+
+  test "renders bakery address, phone, and hours" do
+    get "/contact"
+    assert_select "h1", text: /Contact Us/i
+    assert_select "address", text: /2801 Guilford Ave/
+    assert_match /443-272-1515/, @response.body
+    assert_match /Tues.*Sat/i, @response.body
+  end
 end
