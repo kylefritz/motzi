cbb082da1ada9cafb666898068abae759a88853b
Kyle Fritz <kyle.p.fritz@gmail.com>
2026-09-12 12:08:50 -0700

    Upgrade Rails 8.0.5 to 8.1.3.1, Devise 5, ActiveAdmin 3.5 and patch gem CVEs (squashed) (#360)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require_relative "../config/environment"
 require "rails/test_help"
 require_relative "support/vcr_setup"
 
+# json-schema still defaults to MultiJson and warns about it; use the stdlib JSON directly.
+JSON::Validator.use_multi_json = false
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
